1c9a7cdf67904177026008fe4f79c4730a3336e2
Akhila499 <akhilathota499@gmail.com>
2022-01-05 19:32:22 -0500

    front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import NavBar from "./components/NavBar";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      
     </div>
   );
 }
