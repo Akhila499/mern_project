2635af5483ed1834617add3c8f218454dc0ba153
Akhila499 <akhilathota499@gmail.com>
2022-01-17 19:51:18 -0500

    installations

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+  
+      <Router>
+        <NavBar />
+        <Routes>
+          
+        </Routes>
+      </Router>
+    
   );
 }
 
--- client/src/components/MovieCreate.jsx
+++ client/src/components/MovieCreate.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieCreate = () => {
   return (
     <div>
-      
+      MovieCreate
     </div>
   )
 }
